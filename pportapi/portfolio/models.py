26e9443f7458e164dcac1bd42964ca2e6250d2e6
SabGibson <sabelogibson1@gmail.com>
2023-01-12 10:30:10 +0000

    added comments model

--- pportapi/portfolio/models.py
+++ pportapi/portfolio/models.py
@@ -29,16 +29,34 @@ class Post(models.Model):
         ('D', 'Draft')
     ]
 
-    author = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE,)
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
+    image = models.ImageField()
+    content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=1, choices=STATUS_CHOICES)
+    liked = models.ManyToManyField(Profile, default=None, blank=True)
 
     def __str__(self) -> str:
         return f"{self.title} by {self.author.username}"
 
 
-class Likes(models.Model):
-    liked_post = models.ManyToManyField(Post, name=likes)
+class Comment(models.Model):
+
+    COMMENT_STATUS_CHOICES = [
+        ('P', 'Public'),
+        ('H', 'Hidden')
+    ]
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    content = models.CharField(max_length=255)
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=1, choices=COMMENT_STATUS_CHOICES)
+
+    class Meta:
+        ordering = ("created_on")
+
+    def __str__(self) -> str:
+        return f"{self.author.username} commented on {self.created_on}"
