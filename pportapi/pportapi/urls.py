292033e5698b1d03134fd3a42bc5fae429019fd8
SabGibson <sabelogibson1@gmail.com>
2023-01-12 17:29:04 +0000

    auth endpoint bug resolved

--- pportapi/pportapi/urls.py
+++ pportapi/pportapi/urls.py
@@ -4,6 +4,7 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include('portfolio.urls')),
-    path('auth/', include('djoser.urls')),
-    path('auth/', include('djoser.urls.jwt'))
+    path("auth/", include('djoser.urls')),
+    path("auth/", include('djoser.urls.jwt')),
+
 ]
