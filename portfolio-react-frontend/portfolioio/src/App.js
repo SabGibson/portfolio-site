466aa6039a2c4ff7cd68cfaac9c38b6973adf796
SabGibson <sabelogibson1@gmail.com>
2023-01-18 21:36:05 +0000

    created basic routes and imported material ui

--- portfolio-react-frontend/portfolioio/src/App.js
+++ portfolio-react-frontend/portfolioio/src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import "./App.css";
+
+import Account from "./pages/Account";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import Signup from "./pages/Signup";
+import Projects from "./pages/Projects";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="sign-up" element={<Signup />} />
+          <Route path="profile" element={<Profile />} />
+          <Route path="account" element={<Account />} />
+          <Route path="projects" element={<Projects />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
