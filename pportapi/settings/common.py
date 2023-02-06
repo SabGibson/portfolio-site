af48f723017290a77922a59a37d623bfba618d90
SabGibson <sabelogibson1@gmail.com>
2023-02-06 03:55:23 +0000

    added approeved cors

--- pportapi/settings/common.py
+++ pportapi/settings/common.py
@@ -161,7 +161,14 @@ DJOSER = {
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
-    "http://127.0.0.1:3000"
+    "http://127.0.0.1:3000",
+    "https://delicate-pie-fe91aa.netlify.app",
+    "http://delicate-pie-fe91aa.netlify.app/",
+    "https://pportio.herokuapp.com",
+    "http://pportio.herokuapp.com/",
+    "https://pportio.onrender.com",
+    "http://pportio.onrender.com"
+
 ]
 
 CLOUDINARY_STORAGE = {
