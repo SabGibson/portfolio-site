d980495b8c6c662c5f12a323a355f111f74ffdfb
SabGibson <sabelogibson1@gmail.com>
2023-02-06 03:30:11 +0000

    updated allowed hosts

--- pportapi/settings/dev.py
+++ pportapi/settings/dev.py
@@ -1,7 +1,7 @@
 from .common import *
 SECRET_KEY = "ylw)a9pr@xslm_7wm&+8*2+w^+m4i!p%u7o_!i%f+if0u)ty=-"
 DEBUG = True
-ALLOWED_HOSTS = ['127.0.0.1','localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 DATABASES = {
     "default": {
